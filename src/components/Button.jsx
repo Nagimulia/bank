1593aa1a2f2c3718decf661a56b5556fc5ecc13d
Keneuova Nagima <nagimakeneuova@gmail.com>
2023-05-10 16:31:44 +0600

    first commit

---
+++ src/components/Button.jsx
@@ -0,0 +1,13 @@
+/* eslint-disable react/prop-types */
+const Button = ({ styleTop }) => {
+  return (
+    <button
+      type="button"
+      className={`py-4 px-6 font-poppins font-medium text-[18px] text-primary bg-blue-gradient rounded-[10px] outline-none ${styleTop}`}
+    >
+      Get Started
+    </button>
+  );
+};
+
+export default Button;
